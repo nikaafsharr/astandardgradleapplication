9552a149be5bc3973e0377dc74dd5a0018d8ae69
nikaafsharr <na918@ic.ac.uk>
2020-12-10 11:17:18 +0000

    Emergency Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println("Good Morning, Universe!");
+        int sum =0;
+        for (int count1 =1 ;count1<11;count1++) {
+            System.out.println(count1);
+        }
+
+
     }
+
 }

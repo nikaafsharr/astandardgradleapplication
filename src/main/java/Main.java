2771caa17945c18d5bd269f72e5b768abc81fbe9
nikaafsharr <na918@ic.ac.uk>
2020-12-10 11:28:00 +0000

    deleted the B parts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Good Morning, Universe!");
-        int sum =0;
-        for (int count1 =1 ;count1<11;count1++) {
-            System.out.println(count1);
-        }
+
 
 
     }

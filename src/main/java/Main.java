96a222f14d6e191fc685ec23fea8dba649cbfe5e
nikaafsharr <na918@ic.ac.uk>
2020-12-17 16:47:29 +0000

    Requirement C

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,15 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello, World!");
 
-        for (int i=0; i<11; i++){
+        for (int i=1; i<11; i++){
             System.out.println(i);
         }
+
+        int sum = 0;
+        for (int j=30;j<41;j++){
+            sum = sum+j;
+        }
+        System.out.println(sum);
+
     }
 }
